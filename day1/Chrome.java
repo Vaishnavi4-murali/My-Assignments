524f6c623794ac86189fe4736b71ee9773d5110d
Vaishnavi4-murali <vaishnavimohan161104@gmail.com>
2025-04-27 13:42:13 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args)
+
+	{ float version=91.0f; 
+	System.out.println(version); 
+	String developer="Google";
+	System.out.println(developer);
+	boolean Beta= false;
+	System.out.println(Beta);
+	int releaseyear=2008;
+	System.out.println(releaseyear);
+	char shortcutkey='C';
+	System.out.println(shortcutkey);
+	}
+
+}
